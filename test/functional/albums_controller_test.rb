69d6b5326accb6fb5d1a9e766d3f67175fb66e00
Espen Antonsen <espen@inspired.no>
2009-05-22 14:28:22 +0200

    event?? no no..album!

--- test/functional/albums_controller_test.rb
+++ test/functional/albums_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class EventsControllerTest < ActionController::TestCase
+class AlbumControllerTest < ActionController::TestCase
   # Replace this with your real tests.
   test "the truth" do
     assert true
