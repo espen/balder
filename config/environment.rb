4884cf24514ff8fa5952f78c59566f01085767ad
Espen Antonsen <espen@inspired.no>
2009-08-01 18:38:54 +0200

    problem on server with this gem

--- config/environment.rb
+++ config/environment.rb
@@ -12,7 +12,7 @@ Rails::Initializer.run do |config|
   config.gem "authlogic"
   config.gem 'mime-types', :lib => 'mime/types'
   config.gem "image_science"
-  config.gem "mini_exiftool"
+  #config.gem "mini_exiftool"
   
   config.load_paths += %W( #{RAILS_ROOT}/app/middleware )
 
