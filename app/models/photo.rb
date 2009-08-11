46380d63ad5981d23140cb557fbfdeea39cb1e77
Espen Antonsen <espen@inspired.no>
2009-08-11 10:45:46 +0200

    write temp value first

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -94,8 +94,8 @@ class Photo < ActiveRecord::Base
 
   def create_thumbnails
     return if File.exists?(APP_CONFIG[:thumbs_path] + self.album.path + "/" + self.id.to_s + "_collection" + self.extension)
+    puts "thumb " + self.path_original
     ImageScience.with_image(self.path_original) do |img|
-        puts "thumb " + self.path_original
         img.cropped_thumbnail(200) do |thumb|
           thumb.save APP_CONFIG[:thumbs_path] + self.album.path + "/" + self.id.to_s + "_collection" + self.extension
         end
