3e0deaef67e1aac41fba156f4474c07d595c0bfe
Espen Antonsen <espen@inspired.no>
2009-10-12 00:26:26 +0200

    need those indexes

---
+++ db/migrate/20091011222125_add_indexes.rb
@@ -0,0 +1,29 @@
+class AddIndexes < ActiveRecord::Migration
+  def self.up
+    add_index :albums, :id, :unique => true
+    add_index :collections, :id, :unique => true
+    add_index :photos, :id, :unique => true
+    add_index :tags, :id, :unique => true
+
+    add_index :collection_albums, :collection_id
+    add_index :collection_albums, :album_id
+    add_index :photos, :album_id
+    add_index :photos, :photo_id
+    add_index :photo_tags, :tag_id
+    add_index :photo_tags, :photo_id
+  end
+
+  def self.down
+    remove_index :albums, :id
+    remove_index :collections, :id
+    remove_index :photos, :id
+    remove_index :tags, :id
+
+    remove_index :collection_albums, :collection_id
+    remove_index :collection_albums, :album_id
+    remove_index :photos, :album_id
+    remove_index :photos, :photo_id
+    remove_index :photo_tags, :tag_id
+    remove_index :photo_tags, :photo_id
+  end
+end
