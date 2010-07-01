bf8d3a0f89f48056c2d265584ff16d72de9fb99c
Espen Antonsen <espen@inspired.no>
2010-07-01 12:14:23 +0200

    initial iphone/ipad support

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,3 +3,6 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
+
+Mime::Type.register_alias "text/html", :iphone
+Mime::Type.register_alias "text/html", :ipad
