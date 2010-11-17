63c0ae35c046a0446349b9a1065617b1083b87f2
Rémi Vanicat <vanicat@debian.org>
2010-11-17 17:21:29 +0100

    We want a resource, named account, using the users controller.
    Not a resource with path named users, and using the account controller...

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do
-  resources :account, :as => "users"
+  resource :account, :controller => :users
   match "login", :to => "user_sessions#new", :as => :login
   match "authenticate", :to => "user_sessions#create", :as => :authenticate
   match "logout", :to => "user_sessions#destroy", :as => :logout
