4c3dbcecb652bc529e8f659394fb067b02f49113
Rémi Vanicat <vanicat@debian.org>
2010-11-17 17:22:01 +0100

    Fixe a bug in authlogic.
    See https://github.com/binarylogic/authlogic/issues/issue/101

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -1,3 +1,6 @@
 class UserSession < Authlogic::Session::Base
   # configuration here, see documentation for sub modules of Authlogic::Session
+  def to_key
+    new_record? ? nil : [ self.send(self.class.primary_key) ]
+  end
 end
