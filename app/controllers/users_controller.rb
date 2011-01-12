0cf6d4f725a22c4394b906a71fc83df1a3645936
Espen Antonsen <espen@inspired.no>
2011-01-12 15:09:52 +0800

    should check public access for user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_filter :check_public_access
   before_filter :require_no_user, :only => [:new, :create]
   before_filter :require_user, :only => [:show, :edit, :update, :destroy]
   skip_filter :setup
