7d88a531f0cce11b515df60192edcc455c041422
Espen Antonsen <espen@inspired.no>
2011-04-11 15:55:18 +0800

    save to 'files' to make scanning easier

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -21,7 +21,7 @@ class FileUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     #{}"uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    ENV['STORAGE_PATH'] + "/#{model.album.path}"
+    ENV['STORAGE_PATH'] + "/files/#{model.album.path}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded
