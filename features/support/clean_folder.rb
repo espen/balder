3cf5214de5ad1a03956871641b60b19b3e63f64d
Espen Antonsen <espen@inspired.no>
2011-09-25 12:51:57 +0800

    Remove cucumber and unused files

--- features/support/clean_folder.rb
+++
@@ -1,9 +0,0 @@
-After do
-require "find"
-Find.find( APP_CONFIG[:photos_path] ) { |path|
-  Dir.delete( path ) if path != APP_CONFIG[:photos_path] && File.directory?(path)
-}
-Find.find( APP_CONFIG[:thumbs_path] ) { |path|
-  Dir.delete( path ) if path != APP_CONFIG[:thumbs_path] && File.directory?(path)
-}
-end
--- test/unit/helpers/albums_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class AlbumsHelperTest < ActionView::TestCase
-end
