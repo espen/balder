0f63fde5e1f6d961f55b698c10bf5a5bdb384494
Tom McKenzie <tom@chillidonut.com>
2013-01-21 22:53:36 +1100

    Moved Facebook details to config/balder.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :facebook, '514233915287967', 'c3e63b8953d65c332ebb3a84dc2dc8c0'
+  (provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET']) unless ENV['FACEBOOK_ID'].nil?
 end
