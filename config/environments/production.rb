c1d7514ef2326fbeced650c269ab9abe567c2724
Espen Antonsen <espen@inspired.no>
2014-05-12 12:25:11 +0700

    logget for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,7 @@ Balder::Application.configure do
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
+  config.logger = Logger.new(STDOUT)
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
